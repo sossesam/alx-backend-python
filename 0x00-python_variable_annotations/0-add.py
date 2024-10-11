98e5e2db80cf7475afb43b6f280b180608b91679
sanmi <sossesam@gmail.com>
2024-10-11 08:03:54 +0100

    all

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-"""
-function to add number and
-return float
-"""
-
 
 def add(a: float, b: float) -> float:
+    """
+    function to add number and
+    return float
+    """
     return a + b
