4ae60cfa47d390516b3d75b6427f7a706ce52e2a
sanmi <sossesam@gmail.com>
2024-10-11 08:11:33 +0100

    all

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,9 @@
-def concat(str1: str, str2: str)->str:
+#!/usr/bin/env python3
+"""
+function that concatenates 2 strings and
+returns a string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
     return str1 + str2
