4ddc37fee5c092bf3667c15f6b7f0387b799a97a
sanmi <sossesam@gmail.com>
2024-10-12 09:41:15 +0100

    all

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Description
+
+Parameters-
+
+Return-
+
+Examples-
+
+
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
