296187bb831305ff382cbf733fcf1847a9b96683
sanmi <sossesam@gmail.com>
2024-10-12 10:19:03 +0100

    all

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Description
+
+Parameters-
+
+Return-
+
+Examples-
+
+
+"""
+
+def sum_list(input_list: list[float]) -> float:
+    # returns the sum of all argument in the list
+
+    n: float = 0
+    for x in input_list:
+        n = n + x
+
+    return n
+
+    
+
