2214c327cf73f8e064faca913f61cb8207e00346
sanmi <sossesam@gmail.com>
2024-10-12 11:15:35 +0100

    all

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from typing import List, Union, Tuple
+"""
+Function:
+
+Parameters-
+
+Return-
+
+Examples-
+
+
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple:
+
+    return (k, v**2)
