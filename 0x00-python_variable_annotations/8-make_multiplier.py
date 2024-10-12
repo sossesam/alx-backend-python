f72230deeb6529fd222152db2735c2eee726a262
sanmi <sossesam@gmail.com>
2024-10-12 12:13:58 +0100

    all

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from typing import Callable
+"""
+Function:
+
+Parameters-
+
+Return-
+
+Examples-
+
+
+"""
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ function that returns a callable"""
+    def func(n: float) -> float:
+        return n * multiplier
+    return func
