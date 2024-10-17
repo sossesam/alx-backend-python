a5526f5f3621c188771241e4d22375e9aba40eb9
sanmi <sossesam@gmail.com>
2024-10-17 21:50:50 +0100

    all

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,9 +6,10 @@ This is a function that awaits and delay
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
-    This is a function that awaits and delay 
+    This is a function that awaits and delay
     """
 
     random_num = random.random() * max_delay
