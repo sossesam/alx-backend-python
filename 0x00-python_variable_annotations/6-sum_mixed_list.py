114c108bcd20c209e4f2a63f4ffafd98013c9038
sanmi <sossesam@gmail.com>
2024-10-18 01:06:30 +0100

    all

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,11 @@ funtion for asyncio
 """
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    funtion for asyncio
+    """
 
     n: float = 0
     x: float
