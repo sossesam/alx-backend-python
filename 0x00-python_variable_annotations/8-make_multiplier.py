840198df9dd766cbc9735d1da99c8f26b8ef882f
sanmi <sossesam@gmail.com>
2024-10-18 01:08:05 +0100

    all

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,7 @@ funtion for asyncio
 """
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ function that returns a callable"""
     def func(n: float) -> float:
