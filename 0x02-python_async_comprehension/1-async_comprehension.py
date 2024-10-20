3065fbec85d2a5f767ba2e6b2d8f89da2eaa5c87
sanmi <sossesam@gmail.com>
2024-10-20 21:32:23 +0100

    all

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-
+"""
+1. Async Comprehensions
+"""
 import asyncio
-
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
+    """
+    1. Async Comprehensions
+    """
     result = []
     async for i in async_generator():
         result.append(i)
