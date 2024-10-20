ad658b1493a4d02fe6f349d77cf4f93c17e739a1
sanmi <sossesam@gmail.com>
2024-10-20 21:49:48 +0100

    all

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime():
+    start = time.time()
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    end = time.time()
+
+    return end - start
